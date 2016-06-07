a6ef8b903875abbe916007fcb0c78e63e8818fed
Matt <mtbeaver@users.noreply.github.com>
2016-06-07 11:22:38 -0700

    Update haiku-senseme.groovy

--- smartapps/haiku-senseme.src/haiku-senseme.groovy
+++ smartapps/haiku-senseme.src/haiku-senseme.groovy
@@ -5,18 +5,22 @@ definition(
     description: "Service Manager for discovering Haiku SenseMe Smart fans",
     category: "Fan",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-	iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-	iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
 
 preferences {
     page(name: "deviceDiscovery", title: "Haiku Smart Fan Discovery", nextPage: "deviceDiscovery")
 }    
 
+def _broadcastAddr = ;
+def _port = 31415;
+
 def deviceDiscovery() {
-    // Set the br
-    device.deviceNetworkId
+    
+    // Set the device id to the broadcast address and port
+    device.deviceNetworkId = "";
 }
 
 void sendDiscoveryMessage() {
-    sendHubCommand(new physicangraph.device.HubAction("<ALL;DEVICE;ID;GET>")
-    }
+    sendHubCommand(new physicangraph.device.HubAction("<ALL;DEVICE;ID;GET>", physicalgraph.device.Protocol.LAN))
+}
